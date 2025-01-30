62bbddd3e40091591a3c5937e1d67f7046857c5b
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2025-01-30 17:01:19 -0300

    Concluir a realização dos seguintes exercícios: 04

--- src/exe004.c
+++ src/exe004.c
@@ -9,29 +9,32 @@ Dada uma lista ordenada e um valor alvo, retorne o
 
 //Declaração de constantes
 #define QUANTIDADE_NUMEROS 7
+#define NUMERO_INSERIR 12
+
+//Declaração dos números ordenados
+int numerosOrdenados[QUANTIDADE_NUMEROS] = {1, 4, 6, 7, 10, 14, 18};
 
 //Declaração da função
 int retornaIndiceNumeroOrdem(int *numeros, int numeroEscolhido);
 
-
 int main() {
+    int indiceInserir = retornaIndiceNumeroOrdem(numerosOrdenados, NUMERO_INSERIR);
 
-
-
+    printf("O numero %d pode ser inserido no seguinte indice: %d", NUMERO_INSERIR, indiceInserir);
+    
     return 0;
 }
 
 //Inicialização da função
 int retornaIndiceNumeroOrdem(int *numeros, int numeroEscolhido) {
+    int indice = 0;
+
     for (int i = 0; i < QUANTIDADE_NUMEROS; i++) {
-        int numero = numeros[i];
+        int numeroAnterior = numeros[i], numeroPosterior = numeros[i+1];
 
-
-
-
+        if (numeroAnterior <= numeroEscolhido && numeroEscolhido <= numeroPosterior) {
+            indice = i+1;
+        }
     }
-
-
-
-
+    return indice;
 }
