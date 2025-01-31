72d0a805628305bae799eb3bc29bb358f8a488fc
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2025-01-31 15:57:33 -0300

    Realizar os seguintes exercícios: 06. Porém, ainda sem êxito

---
+++ src/exe006.c
@@ -0,0 +1,36 @@
+/*
+Maximum Subarray
+Dado um array de inteiros, encontre o subarray contíguo que 
+tenha a maior soma e retorne essa soma.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+//Declaração de constantes
+#define QUANTIDADE_NUMEROS 5
+
+//Declaração da função
+int verificaMaiorSomaSubArray(int *numeros);
+
+int main() {
+
+
+    return 0;
+}
+
+//Inicialização da função
+int verificaMaiorSomaSubArray(int *numeros) {
+    int quantidadeSubConjuntos = pow(2, QUANTIDADE_NUMEROS)-1;
+    int somasSubArray[quantidadeSubConjuntos], quantidadeIteracoes = 1;
+    
+    
+    
+    
+
+}
+
+
+
+
